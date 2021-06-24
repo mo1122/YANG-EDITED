3d15a07d0415b72b4034fd855c3105629b00a96b
mo1122 <40466094+mo1122@users.noreply.github.com>
2021-06-25 01:52:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# YANG-EDITED
+<h1 align="center">YANG: Yet Another Nitro Generator</h1>
+
+<p align="center">
+  <a href="https://github.com/Tenclea/YANG/"><img src="https://img.shields.io/github/last-commit/tenclea/yang?style=flat" /></a>
+  <a href="https://github.com/Tenclea/YANG/stargazers"><img src="https://img.shields.io/github/stars/Tenclea/YANG?style=flat" /></a>
+  <br>
+  <b>The most efficient nitro generator and checker you'll ever find.</b><br>
+  Made with ❤ by <b>Tenclea</b>
+  <br>
+  If you liked this project, please consider <b>starring</b> it <3
+</p>
+
+<h2 align="center">Previews</h2>
+
+<p align="center">
+   • Proxy Scrapper & Checker : <br>
+   <img src="https://i.imgur.com/PQElB3e.png" title="YANG - By Tenclea : Proxy Scrapper & Checker"/>
+   <br><br>
+   • Main Nitro Codes Generator : <br>
+   <img src="https://i.imgur.com/4QlDMU9.png" title="YANG - By Tenclea : Main Nitro Codes Generator"/>
+</p>
+
+## Main features
+
+* **Very fast code generator and checker (2000 attempts/minute)**
+* Proxy scrapper and checker
+* Auto nitro-redeem
+* Download fresh proxies while checking codes
+* Full webhook support
+* Real-time stats
+
+## Requirements
+
+* [Node.js](https://nodejs.org/en/)
+* [A Discord token](https://github.com/Tyrrrz/DiscordChatExporter/wiki/Obtaining-Token-and-Channel-IDs#how-to-get-a-user-token)
+
+## Setup
+
+* Clone or download this repository to your computer.
+* Copy your token in the `redeemToken` field in the `config.json` file (use notepad to open it), and edit the other config variables as you like.
+* Paste fresh http(s)/socks proxies into `required/http-proxies.txt`/`required/socks-proxies.txt`.
+* Open up a command prompt in the downloaded folder and type `npm install` to install the requirements.
+* Start the generator by typing `node app.js`.
+* If any error pops out, make sure to fix it before using the generator.
+
+### Config file
+
+These are the "global/config variables" :
+
+* `debugMode` > Prints additional information to the console, like proxies connection errors. (true/false)
+* `redeemToken` > The token the nitro codes will be applied to. (String)
+* `saveWorkingProxies` > Whether to write or not the working proxies to `working_proxies.txt` (recommended). (true/false)
+* `scrapeProxies` > Whether to automatically or not download fresh proxies from online sources. (true/false)
+* `threads` > The number of attempts to run at the same time. (Number)
+* `webhookUrl` > A webhook url to be notified when a code was found. (String)
+
+## Disclaimer
+
+Everything you can see here has been made for educational purposes and as a proof of concept.  
+I do not promote the usage of my tools, and do not take responsibility for any bad usage of this tool.  
+Stealing codes means stealing money from people and is against Discord's TOS. Don't.
---
+++ app.js
@@ -0,0 +1,200 @@
+const
+	chalk = require('chalk'),
+	logger = require('./utils/logger'),
+	ms = require('ms'),
+	needle = require('needle'),
+	{ checkConfig, checkForUpdates, redeemNitro, sendWebhook } = require('./utils/functions'),
+	{ existsSync, readFileSync, watchFile, writeFileSync } = require('fs'),
+	ProxyAgent = require('proxy-agent');
+
+const stats = { downloaded_codes: [], threads: 0, startTime: 0, used_codes: [], version: require('./package.json').version, working: 0 };
+
+console.clear();
+console.log(chalk.magenta(`
+__  _____________   __________
+_ \\/ /__    |__  | / /_  ____/
+__  /__/ /| |_   |/ /_  / __  
+_/ / _/ ___ |/ /|  / / /_/ /  
+/_/  /_/  |_/_/ |_/  \\____/   
+       ${chalk.italic.gray(`v${stats.version} - by Tenclea`)}
+`));
+
+let config = JSON.parse(readFileSync('./config.json'));
+checkConfig(config);
+watchFile('./config.json', () => {
+	config = JSON.parse(readFileSync('./config.json'));
+
+	// Updates logger
+	logger.level = config.debugMode ? 'debug' : 'info';
+
+	logger.info('Updated the config variables.              ');
+	return checkConfig(config);
+});
+
+/* Load proxies, working proxies and removes duplicates */
+const http_proxies = existsSync('./required/http-proxies.txt') ? (readFileSync('./required/http-proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '').map(p => 'http://' + p) : [];
+const socks_proxies = existsSync('./required/socks-proxies.txt') ? (readFileSync('./required/socks-proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '').map(p => 'socks://' + p) : [];
+const oldWorking = existsSync('./working_proxies.txt') ? (readFileSync('./working_proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '') : [];
+let proxies = [...new Set(http_proxies.concat(socks_proxies.concat(oldWorking)))];
+
+process.on('uncaughtException', () => { });
+process.on('unhandledRejection', (e) => { console.error(e); stats.threads > 0 ? stats.threads-- : 0; });
+process.on('SIGINT', () => { process.exit(); });
+process.on('exit', () => { logger.info('Closing YANG... If you liked this project, make sure to leave it a star on github: https://github.com/Tenclea/YANG ! <3'); checkForUpdates(); });
+
+(async () => {
+	checkForUpdates();
+	if (config.scrapeProxies) proxies = [...new Set(proxies.concat(await require('./utils/proxy-scrapper')()))];
+	if (!proxies[0]) { logger.error('Could not find any valid proxies. Please make sure to add some in the \'required\' folder.'); process.exit(); }
+
+	proxies = await require('./utils/proxy-checker')(proxies, config.threads);
+	if (!proxies[0]) { logger.error('All of your proxies were filtered out by the proxy checker. Please add some fresh ones in the \'required\' folder.'); process.exit(); }
+
+	logger.info(`Loaded ${chalk.yellow(proxies.length)} proxies.              `);
+
+	const generateCode = () => {
+		const code = Array.apply(0, Array(16)).map(() => {
+			return ((charset) => {
+				return charset.charAt(Math.floor(Math.random() * charset.length));
+			})('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+		}).join('');
+		return !stats.used_codes.includes(code) || stats.downloaded_codes.indexOf(code) == -1 ? code : generateCode();
+	};
+
+	const checkCode = async (code, proxy, retries = 0) => {
+		logStats();
+		if (!proxy) { stats.threads--; return; }
+
+		const agent = new ProxyAgent(proxy); agent.timeout = 5000;
+		needle.get(
+			`https://discord.com/api/v9/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`,
+			{
+				agent: agent,
+				follow: 10,
+				response_timeout: 10000,
+				read_timeout: 10000,
+				rejectUnauthorized: false,
+			},
+			(err, res, body) => {
+				if (!body.message && !body.subscription_plan) {
+					let timeout = 0;
+					if (retries < 100) {
+						retries++; timeout = 2500;
+						logger.debug(`Connection to ${chalk.grey(proxy)} failed : ${chalk.red(res.statusCode || 'INVALID RESPONSE')}.`);
+					}
+					else {
+						// proxies.push(proxy); // don't remove proxy
+						logger.debug(`Removed ${chalk.gray(proxy)} : ${chalk.red(res.statusCode || 'INVALID RESPONSE')}`);
+						proxy = proxies.shift();
+					}
+
+					logStats();
+					return setTimeout(() => { checkCode(generateCode(), proxy, retries); }, timeout);
+				}
+
+				retries = 0; let p = proxy;
+				stats.used_codes.push(code);
+				if (!working_proxies.includes(proxy)) working_proxies.push(proxy);
+
+				if (body.subscription_plan) {
+					logger.info(`Found a valid gift code : https://discord.gift/${code} !`);
+
+					// Try to redeem the code if possible
+					redeemNitro(code, config);
+
+					if (config.webhookUrl) { sendWebhook(config.webhookUrl, `(${res.statusCode}) Found a \`${body.subscription_plan.name}\` gift code in \`${ms(+new Date() - stats.startTime, { long: true })}\` : https://discord.gift/${code}.`); }
+
+					// Write working code to file
+					let codes = existsSync('./validCodes.txt') ? readFileSync('./validCodes.txt', 'UTF-8') : '';
+					codes += body?.subscription_plan || '???';
+					codes += ` - https://discord.gift/${code}\n=====================================================\n`;
+					writeFileSync('./validCodes.txt', codes);
+
+					stats.working++;
+				}
+				else if (body.message === 'You are being rate limited.') {
+					// timeouts equal to 600000 are frozen. Most likely a ban from Discord's side.
+					const timeout = body.retry_after;
+					if (timeout != 600000) {
+						proxies.push(proxy);
+						logger.warn(`${chalk.gray(proxy)} is being rate limited (${(timeout).toFixed(2)}s), ${proxies[0] === proxy ? 'waiting' : 'skipping proxy'}...`);
+					}
+					else {
+						logger.warn(`${chalk.gray(proxy)} was most likely banned by Discord. Removing proxy...`);
+					}
+					p = proxies.shift();
+				}
+				else if (body.message === 'Unknown Gift Code') {
+					logger.warn(`${code} was an invalid gift code.              `);
+				}
+				logStats();
+				return setTimeout(() => { checkCode(generateCode(), p); }, p === proxy ? (body.retry_after * 1000 || 1000) : 0);
+			});
+	};
+
+	const logStats = () => {
+		// Update title and write stats to stdout
+		const attempts = stats.used_codes.length;
+		const aps = attempts / ((+new Date() - stats.startTime) / 1000) * 60 || 0;
+		process.stdout.write(`Proxies: ${chalk.yellow(proxies.length + stats.threads)} | Attempts: ${chalk.yellow(attempts)} (~${chalk.gray(aps.toFixed(0))}/min) | Working Codes: ${chalk.green(stats.working)}  \r`);
+		process.title = `YANG - by Tenclea | Proxies: ${proxies.length + stats.threads} | Attempts: ${attempts} (~${aps.toFixed(0)}/min) | Working Codes: ${stats.working}`;
+		return;
+	};
+
+	const threads = config.threads > proxies.length ? proxies.length : config.threads;
+	logger.info(`Checking for codes using ${chalk.yellow(threads)} threads.`);
+
+	const working_proxies = [];
+	stats.startTime = +new Date();
+	sendWebhook(config.webhookUrl, 'Started **YANG**.');
+
+	const startThreads = (t) => {
+		for (let i = 0; i < t; i++) {
+			checkCode(generateCode(), proxies.shift());
+			stats.threads++;
+			continue;
+		}
+
+		logger.debug(`Successfully started ${chalk.yellow(t)} threads.`);
+	};
+
+	startThreads(threads);
+
+	setInterval(() => {
+		// Close / restart program if all proxies used
+		if (stats.threads === 0) {
+			logger.info('Restarting using working_proxies.txt list.');
+			proxies = (readFileSync('./working_proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '');
+			if (!proxies[0]) {
+				logger.error('Ran out of proxies.');
+				if (config.webhookUrl) return sendWebhook(config.webhookUrl, 'Ran out of proxies.').then(setTimeout(() => { process.exit(); }, 2500));
+				else return process.exit();
+			}
+			config.saveWorkingProxies = false;
+			return startThreads(config.threads > proxies.length ? proxies.length : config.threads);
+		}
+
+		/* Save working proxies */
+		if (config.saveWorkingProxies) { writeFileSync('./working_proxies.txt', working_proxies.sort(p => p.indexOf('socks')).join('\n')); }
+	}, 5000);
+
+	let addingProxies = false;
+	setInterval(async () => {
+		checkForUpdates(true);
+		if (addingProxies || !config.scrapeProxies) return;
+		else addingProxies = true;
+
+		logger.info('Downloading updated proxies.');
+
+		const new_http_proxies = existsSync('./required/http-proxies.txt') ? (readFileSync('./required/http-proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '').map(p => 'http://' + p) : [];
+		const new_socks_proxies = existsSync('./required/socks-proxies.txt') ? (readFileSync('./required/socks-proxies.txt', 'UTF-8')).split(/\r?\n/).filter(p => p !== '').map(p => 'socks://' + p) : [];
+
+		const newProxies = new_http_proxies.concat(new_socks_proxies.concat(await require('./utils/proxy-scrapper')())).filter(p => !working_proxies.includes(p));
+		const checked = await require('./utils/proxy-checker')(newProxies, config.threads, true);
+		proxies = proxies.concat(checked);
+
+		logger.info(`Added ${checked.length} proxies.`);
+		startThreads(config.threads - stats.threads);
+		addingProxies = false;
+	}, 60 * 60 * 1000);
+})();
